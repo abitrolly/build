401c1b53c5ca85e58e17f043b7d16276f9e066d2
Anatoli Babenia <anatoli@rainforce.org>
2025-12-26 10:08:05 +0300

    Show debug information about backend
    When reporting underlying backend (setuptools) issues, the
    (setuptools) tracker requirement is to mention exact version of
    backend being used. This information is, however, absent from the
    `build` output.
    
    This commmit shows backed spec being used, without version:
    
        $ touch pyproject.toml
        $ python -m build
        * Creating isolated environment: venv+pip...
        build:backend  setuptools.build_meta:__legacy__
        build:python_executable  /tmp/build-env-098a9c2b/bin/python
        * Installing packages in isolated environment:
          - setuptools >= 40.8.0
        ...
    
    Getting version requires running custom code in venv created by
    `build` after backend is installed and it is not clear how to do
    that yet. The code should extract first component from the spec,
    and append `__version__` to it. At least that's the way of doing
    it for `setuptools`.
    
    Related to https://github.com/pypa/build/issues/959

--- src/build/_builder.py
+++ src/build/_builder.py
@@ -37,7 +37,7 @@ from ._exceptions import (
     BuildSystemTableValidationError,
     TypoWarning,
 )
-from ._util import check_dependency, parse_wheel_filename
+from ._util import check_dependency, debug, parse_wheel_filename
 
 
 TYPE_CHECKING = False
@@ -193,6 +193,7 @@ class ProjectBuilder:
         self._build_system = _parse_build_system_table(_read_pyproject_toml(pyproject_toml_path))
 
         self._backend = self._build_system['build-backend']
+        debug("build:backend  " + self._backend)
 
         if backend_paths := self._build_system.get('backend-path'):
             _validate_backend_path(self._source_dir, backend_paths)
@@ -204,6 +205,7 @@ class ProjectBuilder:
             python_executable=self._python_executable,
             runner=self._runner,
         )
+        debug("build:python_executable  " + str(self._python_executable))
 
     @classmethod
     def from_isolated_env(
