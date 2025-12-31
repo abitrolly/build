c7f571266d14a7b042af950270dfb4df04713c9c
Anatoli Babenia <anatoli@rainforce.org>
2025-12-31 05:42:41 +0300

    Use _ctx.log from __main__.py for debug output
        $ python -m build -v
        * Creating isolated environment: venv+pip...
        build:backend  setuptools.build_meta:__legacy__
        build:python_executable  /tmp/build-env-5yw5g814/bin/python
        ...

--- src/build/_builder.py
+++ src/build/_builder.py
@@ -37,7 +37,7 @@ from ._exceptions import (
     BuildSystemTableValidationError,
     TypoWarning,
 )
-from ._util import check_dependency, debug, parse_wheel_filename
+from ._util import check_dependency, parse_wheel_filename
 
 
 TYPE_CHECKING = False
@@ -193,7 +193,7 @@ class ProjectBuilder:
         self._build_system = _parse_build_system_table(_read_pyproject_toml(pyproject_toml_path))
 
         self._backend = self._build_system['build-backend']
-        debug("build:backend  " + self._backend)
+        _ctx.log('backend  ' + self._backend, origin=('build',))
 
         if backend_paths := self._build_system.get('backend-path'):
             _validate_backend_path(self._source_dir, backend_paths)
@@ -205,7 +205,7 @@ class ProjectBuilder:
             python_executable=self._python_executable,
             runner=self._runner,
         )
-        debug("build:python_executable  " + str(self._python_executable))
+        _ctx.log('python_executable  ' + str(self._python_executable), origin=('build',))
 
     @classmethod
     def from_isolated_env(
