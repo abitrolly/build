27e3c8b9cdd214ea8c7f6fdf10ff4421edef1695
Anatoli Babenia <anatoli@rainforce.org>
2026-01-05 14:27:47 +0300

    Add BaseEnv to query build dependency in system environment
    Otherwise this API was only available in venv

--- src/build/env.py
+++ src/build/env.py
@@ -70,6 +70,26 @@ class IsolatedEnv(typing.Protocol):
         """Generate additional env vars specific to the isolated environment."""
 
 
+class BaseEnv:
+    """Common build environment API"""
+
+    @staticmethod
+    def get_package_version(name: str, paths: list[str] = sys.path) -> str | None:
+        """Return package version installed in the environment, or None.
+        paths is a list of site-packages paths for package discovery
+        (used to query packages in venv without activating it).
+        """
+        from ._compat.importlib import metadata
+
+        # importlib discovery API docs are pending
+        # https://github.com/python/cpython/pull/134751
+        try:
+            distribution = next(iter(metadata.Distribution.discover(name=name, path=paths)))
+        except StopIteration:
+            return None
+        return distribution.version
+
+
 def _has_dependency(
     name: str, minimum_version_str: str | None = None, /, **distargs: object
 ) -> importlib_metadata.Distribution | None:
@@ -96,7 +116,7 @@ def _has_dependency(
     return distribution
 
 
-class DefaultIsolatedEnv(IsolatedEnv):
+class DefaultIsolatedEnv(BaseEnv):
     """
     Isolated environment which supports several different underlying implementations.
     """
@@ -140,16 +160,8 @@ class DefaultIsolatedEnv(IsolatedEnv):
         shutil.rmtree(self._path, ignore_errors=True)
 
     def get_package_version(self, name) -> str | None:
-        """Return package version installed in the environment, or None."""
-        from ._compat.importlib import metadata
-
-        # importlib discovery API docs are pending
-        # https://github.com/python/cpython/pull/134751
-        try:
-            distribution = next(metadata.Distribution.discover(name=name, path=[self._env_backend.purelib_dir]))
-        except StopIteration:
-            return None
-        return distribution.version
+        """Get package version installed in the environment, or None."""
+        return BaseEnv.get_package_version(name, paths=[self._env_backend.purelib_dir])
 
     @property
     def path(self) -> str:
